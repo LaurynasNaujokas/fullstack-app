ba14d2b62c4c6549990046e0f02fdbd423b1ff9b
Laurynas <31208060+Skywalker190@users.noreply.github.com>
2018-12-28 20:15:11 +0100

    Update App.js

--- fullstack-app/src/App.js
+++ fullstack-app/src/App.js
@@ -1,5 +1,10 @@
 import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+
+import Create from './components/create';
+import Edit from './components/edit';
+import Index from './components/index';
 
 class App extends Component {
   render() {
