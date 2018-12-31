4678565f3a8d95279c97f42f280343d86a1d87b0
Laurynas <31208060+Skywalker190@users.noreply.github.com>
2018-12-31 01:06:34 +0100

    Showing DB data

--- fullstack-app/src/components/edit.js
+++ fullstack-app/src/components/edit.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-export default class Create extends Component {
+export default class Edit extends Component {
     render() {
         return (
             <div>
