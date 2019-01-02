66ac18faba385c7e5b08a8e92c857e9e8578da5e
Laurynas <31208060+Skywalker190@users.noreply.github.com>
2019-01-02 16:37:23 +0100

    v

--- fullstack-app/api/DB.js
+++ fullstack-app/api/DB.js
@@ -2,4 +2,3 @@ module.exports = {
     DB: 'mongodb://localhost:27017/fullstack-app'
 }
 
-//00101001000001011001
--- fullstack-app/src/components/TableRow.js
+++ fullstack-app/src/components/TableRow.js
@@ -32,11 +32,12 @@ class TableRow extends Component {
                     
                 </td>
                 <td>
-                <Link to={"/edit/"+this.props.obj._id}
-                 className="btn btn-primary">Edit</Link>
+                    <Link to={"/edit/"+this.props.obj._id}
+                    className="btn btn-primary">Edit</Link>
                 </td>
                 <td>
-                    <button onClick={this.delete} className="btn btn-danger">Delete</button>
+                    <button onClick={this.delete}
+                     className="btn btn-danger">Delete</button>
                 </td>
             </tr>
         )
