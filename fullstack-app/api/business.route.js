dba3ae741a7773088e1b2abbf090c24a36794a83
Laurynas <31208060+Skywalker190@users.noreply.github.com>
2019-01-02 16:32:41 +0100

    Update business.route.js

--- fullstack-app/api/business.route.js
+++ fullstack-app/api/business.route.js
@@ -11,7 +11,7 @@ businessRoutes.route('/add').post(function (req, res) {
   let business = new Business(req.body);
   business.save()
     .then(business => {
-      res.status(200).json({'business': 'business in added successfully'});
+      res.status(200).json({'business': 'business is added successfully'});
     })
     .catch(err => {
     res.status(400).send("unable to save to database");
