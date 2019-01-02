dd62a0e011fec2064e6d22f5d865b047c8aa7cc6
Laurynas <31208060+Skywalker190@users.noreply.github.com>
2019-01-02 16:04:59 +0100

    Update TableRow.js

--- fullstack-app/src/components/TableRow.js
+++ fullstack-app/src/components/TableRow.js
@@ -1,8 +1,12 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+
 
 class TableRow extends Component {
     render() {
         return (
+            
             <tr>
                 <td>
                     {this.props.obj.person_name}
@@ -14,13 +18,18 @@ class TableRow extends Component {
                     {this.props.obj.business_gst_number}
                 </td>
                 <td>
-                    <button className="btn btn-primary">Edit</button>
+                    
+                </td>
+                <td>
+                <Link to={"/edit/"+this.props.obj._id}
+                 className="btn btn-primary">Edit</Link>
                 </td>
                 <td>
                     <button className="btn btn-danger">Delete</button>
                 </td>
             </tr>
         )
+        
     }
 }
 
